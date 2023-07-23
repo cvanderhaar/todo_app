b4239c11f68dff0c951156cc47d47f9134acc92f
Clayton Van Der Haar <cmvanderhaar@shoprite.co.za>
2023-07-23 19:48:32 +0200

    Auth layer

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +17,6 @@ use App\Http\Controllers\TodoController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/', [TodoController::class, 'index']);
 Route::get('create', [TodoController::class, 'create']);
 Route::get('details/{todo}', [TodoController::class, 'details']);
@@ -25,3 +24,14 @@ Route::get('edit/{todo}', [TodoController::class, 'edit']);
 Route::post('update/{todo}', [TodoController::class, 'update']);
 Route::get('delete/{todo}', [TodoController::class, 'delete']);
 Route::post('store-data', [TodoController::class, 'store']);
+
+Route::get('/register', 'RegisterController@show')->name('register.show');
+Route::post('/register', 'RegisterController@register')->name('register.perform');
+Route::get('/login', 'LoginController@show')->name('login.show');
+Route::post('/login', 'LoginController@login')->name('login.perform');
+Route::group(['middleware' => ['auth']], function() {
+    /**
+     * Logout Routes
+     */
+    Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+});
